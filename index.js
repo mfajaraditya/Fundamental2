af728410b5a1e23e774914d42fa542133a45b590
M. Fajar Aditya <mfajaraditya13@gmail.com>
2022-03-18 13:40:26 +0800

    coba webpack

--- index.js
+++
@@ -1,4 +0,0 @@
-import main from "./src/view/main.js";
-import "./src/component/footer-bar.js"
-
-document.addEventListener("DOMContentLoaded", main);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./style/style.css";
+import main from "./view/main.js";
+import "./component/footer-bar.js"
+
+document.addEventListener("DOMContentLoaded", main);
